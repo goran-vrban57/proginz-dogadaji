ccff18811a63f24a07a8ac9a3fecb915ec3986da
goran-vrban57 <162477378+goran-vrban57@users.noreply.github.com>
2024-04-11 11:46:44 +0200

    test

--- back/server.js
+++ back/server.js
@@ -4,12 +4,10 @@ const { MongoClient } = require("mongodb");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// MongoDB connection URL
 const mongoURI =
   "mongodb://localhost:27017";
 const dbName = "dogadajDB";
 
-// Connect to MongoDB
 MongoClient.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,22 +16,18 @@ MongoClient.connect(mongoURI, {
     console.log("Connected to MongoDB");
     const db = client.db(dbName);
 
-    // Start server
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
   })
   .catch((err) => {
     console.error("Failed to connect to MongoDB", err);
-    process.exit(1); // Exit with failure
+    process.exit(1);
   });
 
-// Define routes here
-
-// Example route
 app.get("/api/data", async (req, res) => {
   try {
-    const collection = db.collection("data"); // Specify collection name
+    const collection = db.collection("data");
     const data = await collection.find().toArray();
     res.json(data);
   } catch (error) {
