ec62ffcfeb84e2c79795666750264fc14062473f
goran-vrban57 <162477378+goran-vrban57@users.noreply.github.com>
2024-05-26 14:55:14 +0200

    doradica za jest

--- front/tests/prijava.spec.js
+++ front/tests/prijava.spec.js
@@ -1,43 +1,46 @@
-// Import necessary modules and components
 import { mount } from '@vue/test-utils'
-import prijava from '../src/pages/prijava.vue' // Adjust the path as needed
+import prijava from '../src/pages/prijava.vue' 
 
-// Mock axios to simulate API requests
 jest.mock('axios', () => ({
   post: jest.fn(() => Promise.resolve({ data: 'mocked token' }))
 }))
-
+jest.mock('vue3-recaptcha2', () => ({
+  install: jest.fn(),
+  data() {
+    return { response: null };
+  },
+  template: '<div class="mock-recaptcha"></div>'
+}));
 describe('prijava', () => {
-  it('renders login form correctly', () => {
+  it('ispravno učita formu za prijavu', () => {
     const wrapper = mount(prijava)
     expect(wrapper.find('h5').text()).toBe('Prijava')
     expect(wrapper.find('[label="Korisničko ime"]').exists()).toBe(true)
     expect(wrapper.find('[label="Lozinka"]').exists()).toBe(true)
-    expect(wrapper.find('vue-recaptcha').exists()).toBe(true)
+    expect(wrapper.find('.captcha').exists()).toBe(true)
     expect(wrapper.find('router-link[to="registracija"]').exists()).toBe(true)
   })
 
-  it('submits form with valid data and logs in successfully', async () => {
+  it('uspješno unese podatke i prijavi se', async () => {
     const wrapper = mount(prijava)
     wrapper.setData({
       podaci: {
         korisnicko_ime: 'testuser',
         lozinka_korisnika: 'testpassword'
       },
-      captchaProsla: true // Mock passing captcha
+      captchaProsla: true 
     })
+    await wrapper.vm.$nextTick()
     await wrapper.find('form').trigger('submit')
     await wrapper.vm.$nextTick()
     expect(wrapper.vm.$route.path).toBe('/')
     expect(localStorage.setItem).toHaveBeenCalledWith('token', 'mocked token')
   })
 
-  it('displays error message on form submission with invalid data', async () => {
+  it('prikaže grešku ako su nepravilno uneseni podaci', async () => {
     const wrapper = mount(prijava)
     await wrapper.find('form').trigger('submit')
     await wrapper.vm.$nextTick()
     expect(wrapper.find('.q-notification__content').text()).toContain('Niste unijeli sve podatke!')
   })
-
-  // Additional tests for reCAPTCHA component behavior
 })
